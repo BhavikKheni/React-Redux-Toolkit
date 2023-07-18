e4f337c825871206d2e50555920a918bb18ce549
BhavikKheni <kheni.bhavik@gmail.com>
2023-07-18 16:30:20 +0530

    create home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Trending from "./Trending/Trending";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="homePage">
+      <Trending />
+    </div>
+  );
+};
+
+export default Home;
